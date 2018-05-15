237f0d44c78f6433a8c279203b1922f1698cc872
limerence <936611560@qq.com>
2018-05-15 16:13:59 +0800

    finish more basic work

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # bus-data-analysis
 Bus data analysis and application.
 
+百度地图api不好用，换用leaflet + google map实现数据的显示
+现在数据展示的问题不大，接下来的问题怎么对数据进行分析并展示
+leaf.html 是展示结果的网页
+![描点](http://ww1.sinaimg.cn/large/d26395f3gy1frc3bhy28vj20z80qsu0x.jpg)
+
+
+
+
 已经完成数据存储到数据库
 通过百度地图api对坐标的转换和描点
 
--- convert_json.py
+++ convert_json.py
@@ -7,14 +7,26 @@ convert_json
 读取数据库，将需要的经纬度写入json文件给百度api描点用
 """
 import sqlite3
-import json
+import geojson
 
 con = sqlite3.connect('bus.db')
 c = con.cursor()
-result = c.execute("select LONGITUDE, LATITUDE from bus where GPS_TIME like '2017/03/01%'").fetchall()
-location = {'data': list(map(list, result))}
-with open("location.js", 'w') as f:
-    json.dump(location, f)
+result = c.execute(
+    "select CARID, LONGITUDE, LATITUDE, GPS_TIME, STANDSPEED, SPEED from bus where GPS_TIME like '2017/03/16 06%' and CARID='12020'").fetchall()
+print(len(result))
+feature_collection = [geojson.Feature(geometry=geojson.Point(point[1:3]),
+                                      properties={"info": "车辆：{carid} \n"
+                                                         "经纬度：{latlng}\n"
+                                                         "日期：{time}\n"
+                                                         "标准速度：{standspeed}\n"
+                                                         "速度：{speed}".format(carid=point[0],
+                                                                             latlng=point[1:3],
+                                                                             time=point[3],
+                                                                             standspeed=point[4],
+                                                                             speed=point[5])}) for point in result]
+feature_collection.append(geojson.Feature(geometry=geojson.LineString([point[1:3] for point in result])))
+location = geojson.FeatureCollection(feature_collection)
+with open("location.js", 'w', encoding='utf-8') as f:
+    f.write(geojson.dumps(location, indent=4))
 c.close()
 con.close()
-
